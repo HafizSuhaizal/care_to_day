b6c188a0410e69294ddc433bff2946e9b2ef0274
NG WEI HEN <rolandng0001@gmail.com>
2024-01-05 12:39:33 +0800

    homepage added

---
+++ lib/View/homepage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home",style: GoogleFonts.poppins(
+          fontSize: 20, color: Colors.black,
+        ),),
+      ),
+    );
+  }
+}
